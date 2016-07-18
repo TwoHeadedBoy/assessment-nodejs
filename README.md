cee0245bff1f7214d1e98fcc50bf02da04e9ad17
Omar <omar@Fairlight-Excalibur.local>
2016-07-18 14:57:35 -0400

    Initial commit of repo

---
+++ README.md
@@ -0,0 +1,25 @@
+# Raisely NodeJS Assessment
+
+This quick assessment looks at some basic functions of a Javascript serverside application. We're using NodeJS and some Koa middleware to make this work.
+
+The object of this assessment is to make a simple campaign creation tool. It's got one simple form, and all of the HTML and CSS ready to go, the API endpoints have been mapped. But the NodeJS still needs to be written.
+
+## Setting up
+
+ - You'll want to clone this repository
+ - Install, and npm start to get going.
+
+## Instructions
+
+- You'll need to complete each of the supplied routes to make each endpoint work.
+- On the create form, pull in the themes from the supplied JSON file
+- Hook up the campaign creation form fields, and provide for a local image upload
+- Create the /campaign/:id page to render each campaign
+ - Throw an error if you try to navigate to a campaign that does not exist
+
+## Completing the Assessment
+
+- Simply submit a pull request to this repository to submit your work! Each repository is private, and shared only with you.
+- Or, zip it up and send it back to us!
+
+Thanks so much, and good luck!
---
+++ server.js
@@ -0,0 +1,94 @@
+/**
+ * Dependencies.
+ */
+
+var path = require('path'),
+    render = require('./lib/render'),
+    logger = require('koa-logger'),
+    route = require('koa-route'),
+    serve = require('koa-static-folder'),
+    koa = require('koa'),
+    app = koa();
+
+const Jade = require('koa-jade');
+const jade = new Jade({
+    viewPath: './views',
+    app: app
+});
+
+
+// Let's store a database locally, not great for production, obviously, but great for tests.
+
+var database = [];
+
+// Logger
+
+app.use(logger());
+
+// Routes
+
+app.use(route.get('/', campaigns));
+app.use(route.get('/new', newCampaign));
+app.use(route.get('/campaign/:id', campaign));
+app.use(route.post('/create', create));
+
+app.use(serve('./public'));
+
+
+// Route Definitions
+
+function getThemes(callback) {
+    var JsonPath = path.join(__dirname, '/public/themes/themes.json');
+    var themes = null;
+    var fs = require('fs');
+
+    fs.readFile(JsonPath, 'utf8', function (error, fileBuffer) {
+        if (error) return callback(error);
+        themes = JSON.parse(fileBuffer);
+        callback(null, themes);
+    });
+}
+/**
+ * Post listing.
+ */
+
+function *campaigns() {
+    try {
+        getThemes(function (error, themes) {
+            if (error) throw error;
+            render('campaigns.jade', themes, true);
+        });
+    } catch (error) {
+        throw error
+    }
+}
+
+/**
+ * Show new campaign form
+ */
+
+function *newCampaign() {
+
+}
+
+/**
+ * Show a single campaign
+ */
+
+function *campaign(id) {
+
+}
+
+/**
+ * Create a campaign
+ */
+
+function *create() {
+
+}
+
+
+// Listen
+
+app.listen(3000);
+console.log('Server running: http://localhost:3000');
